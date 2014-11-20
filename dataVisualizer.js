abd6c090e353eb41a08abe4275ea714c7f508d20
haydonclore <cac6708@g.rit.edu>
2014-11-20 17:37:06 -0500

    Added some libraries for and began data filtering

--- dataVisualizer.js
+++ dataVisualizer.js
@@ -27,5 +27,56 @@ var line = body.append("svg")
 	.attr("id", "line");
 
 d3.csv("graph_data.csv", function(err, data){
+
+	var countryNameRange = [];
+	var parseDate = d3.time.format("%m/%_d/%Y").parse;
+	
+	//Handle Date formatting and list of countries for imported data
+	data.forEach(function(d){
+		d.Date = parseDate(d.Date);
+		
+		if($.inArray(d.Country, countryNameRange) === -1){
+			countryNameRange.push(d.Country);
+		}
+	});
+	
+	//Use Crossfilter to group data into better sets
+	var xfilter = crossfilter(data);
+	var allData = xfilter.groupAll();
+	
+	//Define dimensions for filtering data
+	var byDate = xfilter.dimension(function(d){	return d.Date;	});
+	var byCountry = xfilter.dimension(function(d){	return d.Country;	});
+	var byType = xfilter.dimension(function(d){	return d.Type;	});
+	
+	//Create groups of data for relevant stats
+	var types = byType.group().reduceSum(function(d){	return d.Value;	});
+	var cases = byDate.group().reduceSum(function(d){
+		if(d.Type === "Cases")
+			return d.Value;
+		return 0;
+	});
+	var deaths = byDate.group.reduceSum(function(d){
+		if(d.Type === "Deaths")
+			return d.Value;
+		return 0;
+	});
+	var countries = byCountry.group().reduceSum(function(d){	return d.Value;	});
+	var totalCases = byDate.group().reduceSum(function(d){
+		if(d.Type === "Cases")
+			return d.TotalValue;
+		return 0;
+	});
+	var totalDeaths = byDate.group().reduceSum(function(d){
+		if(d.Type === "Deaths")
+			return d.TotalValue;
+		return 0;
+	});
+	
+	//Define Date bounds
+	var firstDay = byDate.bottom(1)[0].Date;
+	var lastDay = byDate.top(1)[0].Date;
+	
+	
 	
 });
